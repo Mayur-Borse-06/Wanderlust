2f22d48cee6d704b6713405fd07d04f2ab321931
Mayur Borse <mayurborse9119@gmail.com>
2025-03-17 22:35:21 +0530

    added middlewares

--- index.js
+++ index.js
@@ -6,6 +6,10 @@ const ejsMate = require("ejs-mate");
 const mongoose = require("mongoose");
 const Listing = require("./models/listing");
 const port = 3000;
+const wrapAsync = require("./utils/wrapAsync.js");
+const ExpressError = require("./utils/ExpressError.js");
+const listingSchema = require("./schema.js");
+const { error } = require("console");
 
 main()
 .then(() => {
@@ -25,61 +29,76 @@ app.use(express.urlencoded({extended: true}));
 app.use(methodOverride("_method"));
 app.use(express.static(path.join(__dirname, "public")));
 
+const validateListing = (req, res, next) => {
+    let { error } = listingSchema.validate(req.body);
+    if(error) {
+        let errMsg = error.details.map((el) => { return el.message }).join(",");
+        throw new ExpressError(400, errMsg)
+    } else {
+        next();
+    }
+}
+
 
 //index route
-app.get("/listings", async(req, res) => {
+app.get("/listings", wrapAsync(async(req, res) => {
     const allListings = await Listing.find({});
     res.render("listings/index.ejs", { allListings });
-})
+}))
 
 //create route
 app.get("/listings/new", (req, res) => {
     res.render("listings/new.ejs");
 })
 
-app.post("/listings", async(req, res, next) => {
-    try {
-        let newListing = new Listing(req.body.listing);
-        await newListing.save();
-        res.redirect("/listings");
-    } catch (err) {
-        next(err);
-    }
-})
+app.post("/listings", validateListing, wrapAsync(async(req, res, next) => {
+    let newListing = new Listing(req.body.listing);
+    await newListing.save();
+    res.redirect("/listings");
+
+}))
 
 //show route
-app.get("/listings/:id", async(req, res) => {
+app.get("/listings/:id", wrapAsync(async(req, res) => {
     let { id } = req.params;
     const listing = await Listing.findById(id);
     res.render("listings/show.ejs", { listing })
-})
+}))
 
-//edit route
-app.get("/listings/:id/edit", async(req, res) => {
+//update route
+app.get("/listings/:id/edit", wrapAsync(async(req, res) => {
     let { id } = req.params;
     const listing = await Listing.findById(id);
     res.render("listings/edit.ejs", { listing })
-})
+}))
 
-//update route
-app.put("/listings/:id", async(req, res) => {
+app.put("/listings/:id", wrapAsync(async(req, res) => {
+    if(!req.body.listing) {
+        throw new ExpressError(400, "Please send valid data for listing")
+    }
     let { id } = req.params;
     let updatedListing = req.body.listing;
     await Listing.findByIdAndUpdate(id, {...updatedListing});
     res.redirect(`/listings/${id}`);
-})
+}))
 
 //destroy route
 
-app.delete("/listings/:id", async(req, res) => {
+app.delete("/listings/:id", wrapAsync(async(req, res) => {
     let { id } = req.params;
     await Listing.findByIdAndDelete(id);
     res.redirect("/listings");
-})
+}))
 
 //Error handling middlewares
+
+app.all("*", (req, res, next) => {
+    next(new ExpressError(404, "Page Not Found!"));
+})
+
 app.use((err, req, res, next) => {
-    res.send("Some error occured");
+    let { statusCode = 500, message = "Some error occured please try again later." } = err;
+    res.status(statusCode).render("error.ejs", { err })
 })
 
 
